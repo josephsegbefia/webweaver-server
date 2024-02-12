1095cf5cb46ac02193e75bf766bbfa7067812cd6
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-02-12 21:03:24 +0000

    Complete get all projects and get one

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -13,6 +13,8 @@ router.post('/portfolios/:uniqueIdentifier/projects', async (req, res, next) => 
 
     const foundPortfolio = await Portfolio.findOne({ uniqueIdentifier: uniqueIdentifier });
     const newProject = await Project.create({ title, shortDesc, description, imgUrl, techsUsed, portfolio: foundPortfolio._id});
+    const updatedPortfolio = await Portfolio.findOneAndUpdate({ uniqueIdentifier: uniqueIdentifier }, { $push: { projects: newProject._id }});
+
 
     res.status(200).json({ newProject, message: 'Nice work on adding a new project!'})
 
@@ -22,24 +24,46 @@ router.post('/portfolios/:uniqueIdentifier/projects', async (req, res, next) => 
   }
 });
 
-router.get('/projects', (req, res, next) => {
-  Project.find()
-    .then(allProjects => res.status(200).json(allProjects))
-    .catch(error => res.json(error));
+// GET projects from a portfolio --GET-- /api/portfolios/:uniqueIdentifier/projects
+router.get('/portfolios/:uniqueIdentifier/projects', (req, res, next) => {
+
+  const { uniqueIdentifier } = req.params;
+
+  Portfolio.findOne({ uniqueIdentifier })
+    .populate('projects', '-__v')
+    .select('projects')
+    .then(portfolio => {
+      if (!portfolio) {
+        return res.status(404).json({ message: 'Portfolio not found' });
+      }
+      res.status(200).json(portfolio.projects);
+    })
+    .catch(error => res.status(500).json({ message: 'Internal Server Error', error }));
 })
 
-router.get('/projects/:projectId', (req, res, next) => {
-  const { projectId } = req.params;
+router.get('/portfolios/:uniqueIdentifier/projects/:projectId', (req, res, next) => {
+  const { uniqueIdentifier, projectId } = req.params;
 
-  if(!mongoose.Types.ObjectId.isValid(projectId)){
-    res.status(400).json({ message: 'Specified id is not valid' });
-    return;
-  }
-
-  Project.findById(projectId)
-    .then(project => res.status(200).json(project))
-    .catch(error => res.json(error));
+  Portfolio.findOne({ uniqueIdentifier })
+    .populate({
+      path: 'projects',
+      // Filter projects by the provided project ID
+      match: { _id: projectId },
+      // Exclude __v fields from the populated project
+      select: '-__v'
+    })
+    // Select only the projects field from the Portfolio object
+    .select('projects')
+    .then(portfolio => {
+      if (!portfolio || !portfolio.projects || portfolio.projects.length === 0) {
+        return res.status(404).json({ message: 'Project not found' });
+      }
+      // Return the first (and only) project in the array
+      res.status(200).json(portfolio.projects[0]);
+    })
+    .catch(error => res.status(500).json({ message: 'Internal Server Error', error }));
 });
+
 
 router.put('/projects/:projectId', (req, res, next) => {
   const { projectId } = req.params;
