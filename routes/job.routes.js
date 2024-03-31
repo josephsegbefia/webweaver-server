8962cd1303ce21600c199d3a2b858063f54a943e
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-03-31 23:07:22 +0200

    Add get job route

--- routes/job.routes.js
+++ routes/job.routes.js
@@ -124,4 +124,103 @@ router.get('/portfolios/:uniqueIdentifier/jobs', (req, res, next) => {
 })
 
 
+router.get('/portfolios/:uniqueIdentifier/jobs/:jobId', (req, res, next) => {
+  const { uniqueIdentifier, jobId } = req.params;
+
+  Portfolio.findOne({ uniqueIdentifier })
+    .populate({
+      path: 'jobs',
+      // Filter projects by the provided project ID
+      match: { _id: jobId },
+      // Exclude __v fields from the populated project
+      select: '-__v'
+    })
+    // Select only the jobs field from the Portfolio object
+    .select('jobs')
+    .then(portfolio => {
+      if (!portfolio || !portfolio.jobs || portfolio.jobs.length === 0) {
+        return res.status(404).json({ message: 'Job not found' });
+      }
+      // Return the first (and only) job in the array
+      res.status(200).json(portfolio.jobs[0]);
+    })
+    .catch(error => res.status(500).json({ message: 'Ooops, something went wrong!' }));
+});
+
+
+// router.put('/portfolios/:uniqueIdentifier/projects/:projectId', isAuthenticated, async (req, res, next) => {
+//   try {
+//     const { uniqueIdentifier, projectId } = req.params;
+//   // Extract the fields to be updated from the request body
+//     const { title, shortDesc, techsUsed, description, imgUrl, liveLink, gitHubLink } = req.body;
+//     const portfolio = await Portfolio.findOne({ uniqueIdentifier: uniqueIdentifier}).populate('projects')
+//     if(!portfolio){
+//       return res.status(404).json({ message: 'Portfolio not found!'});
+//     };
+
+//     const projectToUpdate = portfolio.projects.find(project => project._id.toString() === projectId);
+
+//     if(!projectToUpdate){
+//       res.status(404).json({ message: 'Project was not found in your portfolio'})
+//     }
+
+//     projectToUpdate.title = title;
+//     projectToUpdate.shortDesc = shortDesc;
+//     projectToUpdate.description = description;
+//     projectToUpdate.imgUrl = imgUrl;
+//     projectToUpdate.techsUsed = techsUsed;
+//     projectToUpdate.liveLink = liveLink;
+//     projectToUpdate.gitHubLink = gitHubLink;
+//     // projectToUpdate.portfolio = portfolio._id;
+//     await projectToUpdate.save()
+//     // const projectToUpdate = await portfolio.projects
+
+//     res.status(200).json(projectToUpdate);
+
+//   }catch(error){
+//     console.log(error);
+//     res.status(500).json({message: "Ooops something happened!"})
+//   }
+// });
+
+
+
+// router.delete('/portfolios/:uniqueIdentifier/projects/:projectId', isAuthenticated, async (req, res, next) => {
+//   try {
+//     const { uniqueIdentifier, projectId } = req.params;
+
+//     // Find the portfolio based on the unique identifier
+//     const portfolio = await Portfolio.findOne({ uniqueIdentifier }).populate('projects');
+
+//     // Check if the portfolio exists
+//     if (!portfolio) {
+//       return res.status(404).json({ message: 'Portfolio not found!' });
+//     }
+
+//     // Find the index of the project within the portfolio's projects array
+//     const projectIndex = portfolio.projects.findIndex(project => project._id.toString() === projectId);
+
+//     // Check if the project exists in the portfolio
+//     if (projectIndex === -1) {
+//       return res.status(404).json({ message: 'Project not found in your portfolio' });
+//     }
+
+//     // Remove the project from the projects array
+//     portfolio.projects.splice(projectIndex, 1);
+
+//     // Save the updated portfolio without the deleted project
+//     await portfolio.save();
+
+//     // Delete the project from the database
+//     await Project.findByIdAndDelete(projectId);
+
+//     res.status(200).json({ message: 'Project deleted successfully' });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ message: 'Oops! Something went wrong.' });
+//   }
+// });
+
+
+
 module.exports = router;
