96e3618aeea9aa9aafd45cfacf9007bb71577d68
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-04-04 01:26:51 +0200

    Make some origin changes

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ const cookieParser = require("cookie-parser");
 // unless the request if from the same domain, by default express wont accept POST requests
 const cors = require("cors");
 
-const FRONTEND_URL = "https://webweavrr.com/" || "http://localhost:3000";
+const FRONTEND_URL = process.env.ORIGIN || "http://127.0.0.1:5173";
 
 // Middleware configuration
 module.exports = (app) => {
@@ -24,8 +24,8 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      credentials: true,
-      origin: [FRONTEND_URL]
+      // credentials: true,
+      // origin: [FRONTEND_URL]
     })
   );
 
